d238b9ae56f909bd4cf71456d861ffc4aa091a5d
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-07-21 20:46:45 -0700

    get up login for frontend

---
+++ frontend/src/store/session.js
@@ -0,0 +1,50 @@
+import { csrfFetch } from "./csrf";
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+export const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user
+    }
+}
+export const removeUser = () => {
+    return {
+        type: REMOVE_USER,
+    }
+}
+
+// Session thunk
+export const login = (user) => async (dispatch) => {
+    const { email, password } = user;
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+    })
+
+    const data = await response.json();
+    dispatch(setUser(data));
+    return response;
+}
+
+const initialState = { user: null };
+
+export default function sessionReducer(state = initialState, action) {
+    let newState;
+    switch(action.type) {
+        case SET_USER: {
+            newState = {...state};
+            newState.user = action.payload;
+            return newState;
+        }
+        case REMOVE_USER: {
+            newState = {...state};
+            newState[action.user.id] = null;
+            return newState;
+        }
+        default:
+            return state;
+    }
+}
