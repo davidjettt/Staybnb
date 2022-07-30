ccead87f788983d3d1789888c8368af6b8525f52
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-07-29 17:08:28 -0700

    working on footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,11 +1,27 @@
 
+import { ImGithub } from 'react-icons/im';
+import { BsLinkedin } from 'react-icons/bs';
 import './Footer.css';
 
 export default function Footer () {
     return (
         <>
             <div className="footer">
-                {/* Hello */}
+                <div className='footer-contents'>
+                    <p>Developed by David Jetsupphasuk</p>
+                    <div className='my-links'>
+                        <a href='https://github.com/davidjettt'>
+                            <div className='github-icon'>
+                                <ImGithub />
+                            </div>
+                        </a>
+                        <a href='https://www.linkedin.com/in/david-jetsupphasuk-1494a6125/'>
+                            <div className='linkedin-icon'>
+                                <BsLinkedin />
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
         </>
     )
