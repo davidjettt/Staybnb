ea645b58ad6cf232d5be559666c0bdbf87a7d3fe
David Jetsupphasuk <jetsdavid4@gmail.com>
2022-10-15 00:32:07 -0700

    added user profile icons

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import * as sessionActions from '../../store/session';
 import SignupForm from '../SignupFormModal/SignupForm';
 import { Link, useHistory } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
 import './LoginFormModal.css';
 
 export default function LoginFormModal() {
@@ -45,6 +46,14 @@ export default function LoginFormModal() {
         history.push('/');
     };
 
+    let profileIcon
+    if (!sessionUser) {
+        profileIcon = <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{display: 'block', height: 30, width: 30, fill: 'currentcolor', marginLeft: 5}}><path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path></svg>
+    } else {
+        profileIcon = <Avatar sx={{width: 30, height: 30, marginLeft: .5}}>{sessionUser.firstName[0]}</Avatar>
+    }
+
+
     return (
         <div className='dropdown'>
                 <div className='profile-button-container'>
@@ -54,7 +63,7 @@ export default function LoginFormModal() {
                                     <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{display: 'block', fill: 'none', height: 16, width: 16, stroke: 'currentcolor', strokeWidth: 3, overflow: 'visible', marginLeft: 2, marginTop: 1}}><g fill="none" fillRule="nonzero"><path d="m2 16h28"></path><path d="m2 24h28"></path><path d="m2 8h28"></path></g></svg>
                                 </div>
                             <div className="profile-icon">
-                                <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{display: 'block', height: 26, width: 26, fill: 'currentcolor', marginLeft: 5}}><path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path></svg>
+                                {profileIcon}
                             </div>
                         </div>
                     </button>
